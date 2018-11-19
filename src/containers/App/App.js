835e6ae9883b273f14bc4ac1b33134b4893c8f78
Marek <marekficht@gmail.com>
2018-11-19 21:42:12 +0100

    Dividen od small components.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -17,101 +17,180 @@ class MainNav extends Component {
     }
 }
 
+class HomePage extends Component {
+    render() {
+        return (
+            <section className="home-page">
+                <header>Witaj!</header>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, nihil nostrum officiis quae tempore
+                    ullam.</h1>
+                <button className='btn-home-page'>ZACZNIJ TU</button>
+                <div className='circle-effect'>
+                    <span className='wave1'></span>
+                    <span className='wave2'></span>
+                    <span className='wave3'></span>
+                </div>
+            </section>
+        );
+    }
+}
+
+class AboutMePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+        }
+    }
+
+    render() {
+        return (
+            <section className='aboutme-page'>
+                <header className='header'><p>O mnie</p></header>
+                <div className="img-aboutme-page">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at, beatae commodi corporis culpa deserunt.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at, beatae commodi corporis culpa deserunt.</p>
+            </section>
+        );
+    }
+}
+
+class GalleryPage extends Component {
+    render() {
+        return (
+            <section className="gallery-page">
+                <header className='header'><p>Galeria</p></header>
+
+                <article className='gallery'>
+                    <div className="select-gallery"></div>
+                    <p>Galeria z serialu kryminalnego</p>
+                </article>
+                <article className='gallery'>
+                    <p>Galeria z zajec w teatrze</p>
+                    <div className="select-gallery"></div>
+                </article>
+                <article className='gallery'>
+                    <div className="select-gallery"></div>
+                    <p>Galeria z wystepu w Oplu</p>
+                </article>
+
+            </section>
+        );
+    }
+}
+
+class CVPage extends Component {
+    render() {
+        return (
+            <section className="cv-page">
+                <header className='header'><p>CV</p></header>
+
+                <div className="cv">
+                    <button>Pobierz</button>
+                </div>
+
+            </section>
+        );
+    }
+}
+
+class ContactPage extends Component {
+    render() {
+        return (
+            <section className="contact-page">
+                <header className='header'><p>Kontakt</p></header>
+
+                <div className="contact-links">
+                    <div>
+                        <a href="https://www.facebook.com/karolina.ficht" target="_blank">
+                            <div className='svg-fb' ></div>
+                            <p>fb.com/karolina.ficht</p>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="karolinaficht@wp.pl" target="_blank">
+                            <div className='svg-mail' ></div>
+                            <p>karolinaficht@wp.pl</p>
+                        </a>
+                    </div>
+                </div>
+
+                <form action=''>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" placeholder="nick"/>
+                    <label htmlFor="mail">E-mail*</label>
+                    <input type="email" id="mail" placeholder="e-mail*"/>
+                    <textarea name="textarea" placeholder="Leave me a message*" defaultValue={""}/>
+                    <input type="submit" defaultValue="Wyslij"/>
+                </form>
+
+            </section>
+        );
+    }
+}
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className='foot'>
+                <p>ALL RIGHTS RESERVED &copy; 2018</p>
+                <div>
+                    <p>Created by M.F.</p>
+                    <p>Karolina Ficht</p>
+                </div>
+            </footer>
+        );
+    }
+}
+
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentScrollY: 0,
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener(this.handleScroll);
+    }
+
+    handleScroll = (showInScrollY) => {
+        let pageY = window.scrollY;
+        console.log(pageY)
+
+    }
+
+    render() {
     return (
-      <div className="App">
+        <div className="App">
 
-          <MainNav />
+            <MainNav />
 
-          <section className="home-page">
-              <header>Witaj!</header>
-              <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, nihil nostrum officiis quae tempore
-                  ullam.</h1>
-              <button className='btn-home-page'>ZACZNIJ TU</button>
-              <div className='circle-effect'>
-                  <span className='wave1'></span>
-                  <span className='wave2'></span>
-                  <span className='wave3'></span>
-              </div>
-          </section>
+            <main>
+                <HomePage />
 
-          <section className='aboutme-page'>
-              <header className='header'><p>O mnie</p></header>
-              <div className="img-aboutme-page">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-              </div>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at, beatae commodi corporis culpa deserunt.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad at, beatae commodi corporis culpa deserunt.</p>
-          </section>
+                <AboutMePage />
 
-          <section className="gallery-page">
-              <header className='header'><p>Galeria</p></header>
+                <GalleryPage />
 
-              <article className='gallery'>
-                  <div className="select-gallery"></div>
-                  <p>Galeria z serialu kryminalnego</p>
-              </article>
-              <article className='gallery'>
-                  <p>Galeria z zajec w teatrze</p>
-                  <div className="select-gallery"></div>
-              </article>
-              <article className='gallery'>
-                  <div className="select-gallery"></div>
-                  <p>Galeria z wystepu w Oplu</p>
-              </article>
+                <CVPage />
 
-          </section>
+                <ContactPage />
+            </main>
 
-          <section className="cv-page">
-              <header className='header'><p>CV</p></header>
+            <Footer />
 
-              <div className="cv">
-                  <button>Pobierz</button>
-              </div>
-
-          </section>
-
-          <section className="contact-page">
-              <header className='header'><p>Kontakt</p></header>
-
-              <div className="contact-links">
-                  <div>
-                      <a href="https://www.facebook.com/karolina.ficht" target="_blank">
-                          <div className='svg-fb' ></div>
-                          <p>fb.com/karolina.ficht</p>
-                      </a>
-                  </div>
-                  <div>
-                      <a href="karolinaficht@wp.pl" target="_blank">
-                          <div className='svg-mail' ></div>
-                          <p>karolinaficht@wp.pl</p>
-                      </a>
-                  </div>
-              </div>
-
-              <form action=''>
-                  <label htmlFor="name">Name</label>
-                  <input type="text" id="name" placeholder="nick"/>
-                  <label htmlFor="mail">E-mail*</label>
-                  <input type="email" id="mail" placeholder="e-mail*"/>
-                  <textarea name="textarea" placeholder="Leave me a message*" defaultValue={""}/>
-                  <input type="submit" defaultValue="Wyslij"/>
-              </form>
-
-          </section>
-
-          <footer className='foot'>
-              <p>ALL RIGHTS RESERVED &copy; 2018</p>
-              <div>
-                  <p>Created by M.F.</p>
-                  <p>Karolina Ficht</p>
-              </div>
-          </footer>
-      </div>
+        </div>
     );
   }
 }
